546b47f18fb1686e96a84bc7fcf1c10ba9c616f7
John <jonathan.plantey@gmail.com>
2024-01-19 14:57:39 +0100

    wip

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,12 @@
 /*
  * Welcome to your app's main JavaScript file!
  *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
  */
-
-// any CSS you import will output into a single css file (app.css in this case)
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './css/app.css';
 
 // js
-import 'bootstrap';
+// import 'bootstrap';
--- docker/apache/docker-entry.sh
+++ docker/apache/docker-entry.sh
@@ -1,4 +1,5 @@
 php bin/console doctrine:database:create --if-not-exists
 php bin/console d:m:m --no-interaction
+php bin/console asset-map:compile
 
 /usr/bin/supervisord
